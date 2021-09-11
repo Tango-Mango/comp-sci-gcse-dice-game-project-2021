61d1a5097342c516f1c2b6213cf6286b18c874aa
Tangliang Li <81322923+Tango-Mango@users.noreply.github.com>
2021-09-11 02:11:30 +0100

    init

--- script.js
+++ script.js
@@ -241,3 +241,35 @@ roll = () => {
 
 
 //Leaderboard code
+//Firebase code
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC9Z0mwHgIFizO5EPtCA31_2pL44FyXesI",
+  authDomain: "comp-sci-gcse-dice-game.firebaseapp.com",
+  projectId: "comp-sci-gcse-dice-game",
+  storageBucket: "comp-sci-gcse-dice-game.appspot.com",
+  messagingSenderId: "289504905992",
+  appId: "1:289504905992:web:e499d49774ab6b988f7abd",
+  measurementId: "G-4GP3VGB5DG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+let database = firebase.database();
+let ref = database.ref('scores');
+
+let data = {
+  name: "",
+  score: ""
+}
+
+ref.push(data);
